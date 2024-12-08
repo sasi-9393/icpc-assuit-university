c8da015b31ee86cfba47de33fb250d4147ad4773
sasi-9393 <mvskumar9393@gmail.com>
2024-12-08 10:50:52 +0530

    Solutions

---
+++ CP/A_Create_A_New_String.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+// Modular Arithmetic Constant
+const int MOD = 1000000007;
+
+// Mathematical Constant Pi
+const double PI = 3.141592653589793;
+
+#define ll long long
+
+// Debugging macros
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+
+// Output macros
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+// Custom endl macro for faster output
+#define endl '\n'
+
+// Base Template for Debugging
+template <typename T>
+void _print(const T& value) {
+    cerr << value;  // Default print for anything that's not a container
+}
+
+// Specialization for pair
+template <typename T, typename V>
+void _print(const pair<T, V>& p) {
+    cerr << "{ ";
+    _print(p.first);
+    cerr << ", ";
+    _print(p.second);
+    cerr << " }";
+}
+
+// Specialization for vector
+template <typename T>
+void _print(const vector<T>& v) {
+    cerr << "[ ";
+    for (const T& elem : v) {
+        _print(elem);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+// Specialization for 2D vector
+template <typename T>
+void _print(const vector<vector<T>>& v) {
+    cerr << "[\n";
+    for (const auto& row : v) {
+        _print(row);
+        cerr << "\n";
+    }
+    cerr << "]";
+}
+
+// Specialization for C-style array
+template <typename T, size_t N>
+void _print(const T (&arr)[N]) {
+    cerr << "[ ";
+    for (size_t i = 0; i < N; ++i) {
+        _print(arr[i]);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+// Specialization for 2D C-style array
+template <typename T, size_t R, size_t C>
+void _print(const T (&arr)[R][C]) {
+    cerr << "[\n";
+    for (size_t i = 0; i < R; ++i) {
+        cerr << "  [ ";
+        for (size_t j = 0; j < C; ++j) {
+            _print(arr[i][j]);
+            cerr << " ";
+        }
+        cerr << "]\n";
+    }
+    cerr << "]";
+}
+
+// Specialization for set
+template <typename T>
+void _print(const set<T>& s) {
+    cerr << "{ ";
+    for (const T& elem : s) {
+        _print(elem);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// Specialization for unordered_set
+template <typename T>
+void _print(const unordered_set<T>& us) {
+    cerr << "{ ";
+    for (const T& elem : us) {
+        _print(elem);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// Specialization for map
+template <typename K, typename V>
+void _print(const map<K, V>& m) {
+    cerr << "{ ";
+    for (const auto& pair : m) {
+        _print(pair);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// Specialization for unordered_map
+template <typename K, typename V>
+void _print(const unordered_map<K, V>& m) {
+    cerr << "{ ";
+    for (const auto& pair : m) {
+        _print(pair);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// Main function
+int main() {
+    FAST_IO;
+
+    string a,b;
+    cin>>a>>b;
+
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<a<<" "<<b<<endl;
+
+    return 0;
+}
---
+++ CP/C_Replacement.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+// Modular Arithmetic Constant
+const int MOD = 1000000007;
+
+// Mathematical Constant Pi
+const double PI = 3.141592653589793;
+
+#define ll long long
+
+// Debugging macros
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+
+// Base Template for Debugging
+template <typename T>
+void _print(const T& value) {
+    cerr << value;  // Default print for anything that's not a container
+}
+
+// Specialization for pair
+template <typename T, typename V>
+void _print(const pair<T, V>& p) {
+    cerr << "{ ";
+    _print(p.first);
+    cerr << ", ";
+    _print(p.second);
+    cerr << " }";
+}
+
+// Specialization for vector
+template <typename T>
+void _print(const vector<T>& v) {
+    cerr << "[ ";
+    for (const T& elem : v) {
+        _print(elem);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+// Specialization for set
+template <typename T>
+void _print(const set<T>& s) {
+    cerr << "{ ";
+    for (const T& elem : s) {
+        _print(elem);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// Specialization for map
+template <typename K, typename V>
+void _print(const map<K, V>& m) {
+    cerr << "{ ";
+    for (const auto& pair : m) {
+        _print(pair);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// Specialization for unordered_map
+template <typename K, typename V>
+void _print(const unordered_map<K, V>& m) {
+    cerr << "{ ";
+    for (const auto& pair : m) {
+        _print(pair);
+        cerr << " ";
+    }
+    cerr << "}";
+}
+
+// Main function
+int main() {
+    FAST_IO;
+
+    // Write your code here
+    int n;
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0;i<n;++i)cin>>arr[i];
+
+    for(int i=0;i<n;++i){
+        if(arr[i]>0)arr[i]=1;
+        else if(arr[i]<0)arr[i]=2;
+    }
+
+    for(auto i:arr)cout<<i<<" ";
+
+    return 0;
+}
